45875c6d70c277ccb682ad3da39732ccbe46df2f
ChrisHIV <15834752+ChrisHIV@users.noreply.github.com>
2026-04-27 12:45:13 +0100

    Allow gzipped input

--- R/dvsb_read_cmdstan_out_files.R
+++ R/dvsb_read_cmdstan_out_files.R
@@ -7,13 +7,19 @@ read_cmdstan_out_files <- function(file_paths,
   stopifnot(length(file_paths) > 0)
   stopifnot(all(file.exists(file_paths)))
   stopifnot(is.character(params_to_ignore))
-  
-  df_fit_wide_postonly <- map(file_paths, function(file_){
+  mastiff::check_numeric(downsampling_factor, lower = 1, upper_inclusive = FALSE)
+
+  map(file_paths, function(file_){
     if (verbose) {
       print(Sys.time())
       cat("Now reading file", file_, "\n")
     }
-    df_ <- data.table::fread(cmd = paste("grep -v '^#'", file_))
+    if (endsWith(file_, "gz")) {
+      cmd <- paste("gunzip -c", file_, "| grep -v '^#'")
+    } else {
+      cmd <- paste("grep -v '^#'", file_)
+    }
+    df_ <- data.table::fread(cmd = cmd)
     if (length(params_to_ignore)) {
       keep_col <- rep(TRUE, ncol(df_))
       for (param in params_to_ignore) {
@@ -29,5 +35,5 @@ read_cmdstan_out_files <- function(file_paths,
     }
     df_
   }) %>% data.table::rbindlist()
-  
+
 }
