773cf464a8528e89dd5ffa66902d189e63821d9d
ChrisHIV <15834752+ChrisHIV@users.noreply.github.com>
2026-08-26 17:13:23 +0100

    Revert column selection to logical-vector-based, since without the .. syntaxm data.table complains regardless

--- R/dvsb_run_stan_interfaces.R
+++ R/dvsb_run_stan_interfaces.R
@@ -245,8 +245,7 @@ run_stan_interfaces <- function(input_to_stan,
       ! startsWith(colnames(df_samples), paste0(param, "["))
     keep_col <- keep_col & keep_based_on_this_param
   }
-  cols_to_keep <- names(df_samples)[keep_col]
-  df_samples <- df_samples[, cols_to_keep]
+  df_samples <- df_samples[, ..keep_col]
   
   df_samples
   
